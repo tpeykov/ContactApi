ce00c4451a02673e8ceca739792ec86cde5b708b
tpeykov <ter4it_123@abv.bg>
2024-06-10 15:37:07 +0300

    Started version!

---
+++ src/main/java/com/main/api/ContactApi/ContactApiApplication.java
@@ -0,0 +1,13 @@
+package com.main.api.ContactApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/api/ContactApi/ContactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.api.ContactApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
